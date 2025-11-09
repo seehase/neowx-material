0d5da4c2fc073af735a572ca5da7f97a23beb233
Holger Seehausen <seehausen@mediamarktsaturn.com>
2025-11-09 13:48:56 +0100

    feat: reformat *.inc files

--- install.py
+++ install.py
@@ -10,7 +10,7 @@ def loader():
 class BasicInstaller(ExtensionInstaller):
     def __init__(self):
         super(BasicInstaller, self).__init__(
-            version="1.52.10",
+            version="1.52.11",
             name="neowx-material",
             description="The most versatile and modern weewx skin",
             author="Neoground GmbH",
