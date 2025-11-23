791883311bfde1a52a54272d55d8a6dc2b501057
Tomáš Filo <tfilosk@gmail.com>
2025-11-23 08:55:00 +0100

    Possible fix for #214, need more testing

--- install.py
+++ install.py
@@ -10,7 +10,7 @@ def loader():
 class BasicInstaller(ExtensionInstaller):
     def __init__(self):
         super(BasicInstaller, self).__init__(
-            version="1.52.18",
+            version="1.52.19",
             name="neowx-material",
             description="The most versatile and modern weewx skin",
             author="Neoground GmbH",
