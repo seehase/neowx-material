942a03318fb3722ccaaaed437421f4e910ba685e
Holger Seehausen <seehausen@mediamarktsaturn.com>
2026-02-15 12:16:07 +0100

    fix: Correct chart background color in darkmode

--- install.py
+++ install.py
@@ -10,7 +10,7 @@ def loader():
 class BasicInstaller(ExtensionInstaller):
     def __init__(self):
         super(BasicInstaller, self).__init__(
-            version="1.52.26",
+            version="1.52.27",
             name="neowx-material",
             description="The most versatile and modern weewx skin",
             author="Neoground GmbH",
